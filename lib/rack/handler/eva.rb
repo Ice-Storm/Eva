a5c80b2b1f87367be4f94af25badac297bc2bbd2
hehao <hehao@linkface.cn>
2017-11-29 16:30:48 +0800

    Add simple rack handler

--- lib/rack/handler/eva.rb
+++ lib/rack/handler/eva.rb
@@ -1,11 +1,26 @@
 require 'rack/handler'
+require 'eva/launcher'
+require 'eva/configuration'
+require 'eva/events'
 
 module Rack
   module Handler
     module Eva
 
+      def self.config(app, options={})
+        conf = ::Eva::Configuration.new(options, {}) do |user_config, file_config, default_config|
+          user_config.app(app)
+        end
+        conf
+      end
+
       def self.run(app, options = {})
-        ::Eva::Server.new(app).run
+        conf = self.config(app, options)
+        events = ::Eva::Events.strings
+        launcher = ::Eva::Launcher.new(conf, :events => events)
+
+        launcher.run
+        #::Eva::Server.new(app).run
       end
 
     end
